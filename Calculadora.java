d1f4eab9d07fb1265dcf030d03fd05634e0c6c9e
Pedro Rodríguez Amaro <prodriguezamaro@colegiolagomar.es>
2024-11-18 19:27:38 +0100

    Agregar método multiplicar en la clase Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    // Método existente
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Nuevo método
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
